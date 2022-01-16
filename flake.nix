755fd9ce06290c7fcc14362d881b52030bba8416
bandithedoge <bandithedoge@protonmail.com>
2022-01-16 13:08:04 +0100

    use flake-compat

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
